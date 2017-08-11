e70e47d95e346235d7030d7c08c1f0783cc51434
Olga <o.dudko@cybetech.com>
2017-08-11 11:39:07 +0300

    Player & Enemy objects and moving

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ window.onload = init;
 
 var map, ctxMap;
 var player, ctxPlayer;
+var enemy, ctxEnemy;
 var drawBtn, clearBtn;
 var gameWidth = 800,
     gameHeight = 500;
@@ -12,12 +13,26 @@ bg.src = 'images/bg.jpg';
 var tiles = new Image();
 tiles.src = 'images/sprite.png';
 
+var playerObj,
+    enemyObj;
+
+var isPlaying;
+
+var requestAnimFrame = window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
+
 function init() {
     map = document.getElementById('map');
     ctxMap = map.getContext('2d');
 
     player = document.getElementById('player');
-    ctxPlayer = map.getContext('2d');
+    ctxPlayer = player.getContext('2d');
+
+    enemy = document.getElementById('enemy');
+    ctxEnemy = enemy.getContext('2d');
 
     map.width = gameWidth;
     map.height = gameHeight;
@@ -25,15 +40,86 @@ function init() {
     player.width = gameWidth;
     player.height = gameHeight;
 
+    enemy.width = gameWidth;
+    enemy.height = gameHeight;
+
     drawBtn = document.getElementById('drawBtn');
     clearBtn = document.getElementById('clearBtn');
 
     drawBtn.addEventListener('click', drawRect, false);
     clearBtn.addEventListener('click', clearRect, false);
 
+    playerObj = new Player();
+    enemyObj = new Enemy();
+
     drawBg();
-    drawPlayer();
+
+    startLoop();
 }
+
+function draw() {
+    playerObj.draw();
+    enemyObj.draw();
+}
+
+function update() {
+    playerObj.update();
+}
+
+function loop() {
+    if (isPlaying) {
+        draw();
+        update();
+        requestAnimFrame(loop);
+    }
+}
+
+function startLoop() {
+    isPlaying = true;
+    loop();
+}
+
+function stopLoop() {
+    isPlaying = false;
+}
+
+// Objects
+function Player() {
+    this.srcX = 0;
+    this.srcY = 0;
+    this.drawX = 0;
+    this.drawY = 0;
+    this.width = 200;
+    this.height = 147;
+
+    this.speed = 5;
+}
+
+function Enemy() {
+    this.srcX = 0;
+    this.srcY = 147;
+    this.drawX = 600;
+    this.drawY = 50;
+    this.width = 164;
+    this.height = 147;
+
+    this.speed = 8;
+}
+
+Player.prototype.draw = function () {
+    clearCtxPlater();
+    ctxPlayer.drawImage(tiles, this.srcX, this.srcY, this.width, this.height,
+        this.drawX, this.drawY, this.width, this.height);
+};
+
+Player.prototype.update = function () {
+    this.drawX += this.speed;
+};
+
+Enemy.prototype.draw = function () {
+    ctxEnemy.drawImage(tiles, this.srcX, this.srcY, this.width, this.height,
+        this.drawX, this.drawY, this.width, this.height);
+};
 
 function drawRect() {
     ctxMap.fillStyle = '#3d3d3d';
@@ -44,12 +130,11 @@ function clearRect() {
     ctxMap.clearRect(0, 0, 800, 500);
 }
 
+function clearCtxPlater() {
+    ctxPlayer.clearRect(0, 0, gameWidth, gameHeight);
+}
+
 function drawBg() {
     ctxMap.drawImage(bg, 0, 0, 1600, 1000,
         0, 0, gameWidth, gameHeight);
-}
-
-function drawPlayer() {
-    ctxPlayer.drawImage(tiles, 0, 0, 200, 147,
-        0, 0, 200, 147);
 }
